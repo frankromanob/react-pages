62c94e6c09ff7b71f3afb2bb46385dca5033d179
Frank <romano.fr@gmail.com>
2022-09-29 16:24:58 -0400

    Updates

---
+++ precache-manifest.d7366d861e5f7b617a16aedbba1793f1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b32c098250db74df53943b9508651ef4",
+    "url": "/react-pages/index.html"
+  },
+  {
+    "revision": "4215a8f3ba90cbffb84c",
+    "url": "/react-pages/static/css/main.c82dc1f3.chunk.css"
+  },
+  {
+    "revision": "a41e2614c7681499b689",
+    "url": "/react-pages/static/js/2.ed5edad8.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-pages/static/js/2.ed5edad8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4215a8f3ba90cbffb84c",
+    "url": "/react-pages/static/js/main.f77a3eef.chunk.js"
+  },
+  {
+    "revision": "469f983af278f907605b",
+    "url": "/react-pages/static/js/runtime-main.bebf4745.js"
+  }
+]);
